49934060cc43b996f858523d5dd47c4f131804f4
vivimila <violamilashevskaya@yandex.by>
2022-09-06 21:30:52 +0300

    Task 66 is in progress

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -4,3 +4,20 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите значение числа M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение числа N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+void NaturalNumber(int sum)
+{
+    if (numberM < numberN) return;
+    sum = sum + (numberM++);
+    //Console.Write($"Сумма от M до N: {sum} ");
+   
+}
+
+Console.Write($"Сумма от M до N: {sum} ");
+
+NaturalNumber(sum);

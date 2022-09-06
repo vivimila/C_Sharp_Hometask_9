3f6dcec4e77e2034d6e15245258ad66db15b6c69
vivimila <violamilashevskaya@yandex.by>
2022-09-06 20:21:42 +0300

    Task 64 is solved

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -3,3 +3,15 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumber(num - 1);  
+}
+
+NaturalNumber(number);

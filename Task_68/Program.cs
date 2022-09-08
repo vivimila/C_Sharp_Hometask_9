0a9019a77fbb5a5c7f1453b451a60c57cccd71bc
vivimila <violamilashevskaya@yandex.by>
2022-09-08 17:09:48 +0300

    Task 68 is in progress

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -3,3 +3,18 @@
 
 // m = 3, n = 2 -> A(m,n) = 29
 
+Console.Write("Введите число m: ");
+int m = 3; 
+Console.Write("Введите число n: ");
+int n = 2; 
+
+//int Akk(n, m);
+
+int Akk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m != 0) && (n == 0)) return Akk(m - 1, 1);
+    else if ((m > 0) && (n > 0)) return Akk(m - 1, Akk(m, n - 1));
+    return Akk(m, n);
+}
+Console.WriteLine($"Функция Аккермана  = {Akk(n, m)}");

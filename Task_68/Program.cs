b1c290eb22848148d5e16b7487f1b607bedc6fa1
vivimila <violamilashevskaya@yandex.by>
2022-09-08 22:16:14 +0300

    Task 68 is solved

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -4,9 +4,9 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 Console.Write("Введите число m: ");
-int m = 3; 
+int m = Convert.ToInt32(Console.ReadLine()); 
 Console.Write("Введите число n: ");
-int n = 2; 
+int n = Convert.ToInt32(Console.ReadLine());
 
 //int Akk(n, m);
 
@@ -17,4 +17,4 @@ int Akk(int m, int n)
     else if ((m > 0) && (n > 0)) return Akk(m - 1, Akk(m, n - 1));
     return Akk(m, n);
 }
-Console.WriteLine($"Функция Аккермана  = {Akk(n, m)}");
+Console.WriteLine($"Функция Аккермана  = {Akk(m, n)}");
